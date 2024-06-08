3b3869e53a38f6ffd55bafd441374ab94eaa7c42
abhay <abhayforwork02@gmail.com>
2024-06-08 23:28:24 +0530

    code

---
+++ config/data.js
@@ -0,0 +1,12 @@
+import mongoose, { connect } from "mongoose";
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("connected to MongoDB");
+    } catch (error) {
+        console.error(`ERROR: ${error.message}`);
+        process.exit(1);
+    }
+}
+export default connectDB;
